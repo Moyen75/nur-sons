e3612d69709e829a5abefd53cdcf27175c2ad84d
moyen75 <moyenislam75@.com>
2021-11-13 21:09:06 +0600

    complete

--- src/pages/Dashboard/Admin/Users/Users.js
+++ src/pages/Dashboard/Admin/Users/Users.js
@@ -1,10 +1,25 @@
-import { TableCell, TableRow, Typography } from '@mui/material';
+import { Button, TableCell, TableRow, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
 import useUsers from '../../../../hooks/useUsers';
 
 const Users = () => {
     const users = useUsers()
+
+    const handleDelete = id => {
+        const process = window.confirm('Are you sure to delete this user??')
+        if (process) {
+            fetch(`http://localhost:5000/users/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('user deleted successfully.')
+                    }
+                })
+        }
+    }
     return (
         <Box>
             <Typography variant='h4' sx={{ textAlign: 'left' }}>
@@ -13,6 +28,7 @@ const Users = () => {
             {users?.map(user => <TableRow>
                 <TableCell> {user.email}</TableCell>
                 {user.role && <TableCell>{user.role}</TableCell>}
+                {!user.role && <TableCell> <Button onClick={() => handleDelete(user._id)}>Delete</Button> </TableCell>}
             </TableRow>)}
         </Box>
     );
